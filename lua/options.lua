799fda1cdeed916abd9135b0eb9765d1a59647f6
Igor Silvestrin <silvestrinigor@icloud.com>
2026-01-21 13:00:57 -0300

    commit

--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,9 @@ require "nvchad.options"
 
 -- add yours here!
 
+vim.opt.relativenumber = true
+-- Keep cursor centered
+vim.opt.scrolloff = 999
+vim.opt.termguicolors = true
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
